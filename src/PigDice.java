472d1a41691cdfc843a5b05149d8ffaf3c5538ad
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2023-10-31 09:47:18 -0400

    Update PigDice.java

--- src/PigDice.java
+++ src/PigDice.java
@@ -2,24 +2,45 @@ public class PigDice {
     //Your code here
 
 
+
+
+
+
+
 	
-	public String toString() {
-		String d1 = "";
-		if(first == 1) d1 = "\u2680";
-		if(first == 2) d1 = "\u2681";
-		if(first == 3) d1 = "\u2682";
-		if(first == 4) d1 = "\u2683";
-		if(first == 5) d1 = "\u2684";
-		if(first == 6) d1 = "\u2685";
-		
-		String d2 = "";
-		if(second == 1) d2 = "\u2680";
-		if(second == 2) d2 = "\u2681";
-		if(second == 3) d2 = "\u2682";
-		if(second == 4) d2 = "\u2683";
-		if(second == 5) d2 = "\u2684";
-		if(second == 6) d2 = "\u2685";
-		
-		return d1 + " " + d2;		
-	}
+    // POST: returns a String formatted to display two dice visually side by side
+    public String toString() {
+        if (isValidDieNumber(first) && isValidDieNumber(second)) {
+            // Define the ASCII art for each side of a die
+            String[] dice = {
+                    " -------\n|       |\n|   *   |\n|       |\n -------",
+                    " -------\n|     * |\n|   *   |\n| *     |\n -------",
+                    " -------\n| *     |\n|   *   |\n|     * |\n -------",
+                    " -------\n| *   * |\n|       |\n| *   * |\n -------",
+                    " -------\n| *   * |\n|   *   |\n| *   * |\n -------",
+                    " -------\n| *   * |\n| *   * |\n| *   * |\n -------"
+            };
+
+            // Display the dice side by side
+            String[] lines1 = dice[first - 1].split("\n");
+            String[] lines2 = dice[second - 1].split("\n");
+
+            String result = "";
+            for (int i = 0; i < 5; i++) {
+                if (i == 0 || i == 4) {
+                    result += lines1[i] + "     " + lines2[i] + "\n";
+                } else {
+                    result += lines1[i] + "    " + lines2[i] + "\n";
+                }
+            }
+            return result;
+        } else {
+            return "Invalid die numbers. Please use numbers between 1 and 6.";
+        }
+    }
+
+    // POST: returns true if num is a valid dice value, false otherwise
+    private boolean isValidDieNumber(int num) {
+        return num >= 1 && num <= 6;
+    }
 }
